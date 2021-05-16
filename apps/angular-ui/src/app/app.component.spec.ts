84301baa4137de72a8af4c0ca714478625855fc9
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-05-16 16:51:13 +0200

    Moving to generic NX project

--- apps/angular-ui/src/app/app.component.spec.ts
+++ apps/angular-ui/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -24,8 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to angular-ui!'
-    );
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-ui!');
   });
 });
--- apps/angular-ui/src/app/app.module.ts
+++ apps/angular-ui/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot([], { initialNavigation: 'enabled' })],
   providers: [],
   bootstrap: [AppComponent],
 })
