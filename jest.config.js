cffaa4c1eb7b13e8e99bf025c6c8dbb195c06cdb
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-05-17 20:28:59 +0200

    fix jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   projects: [
     '<rootDir>/apps/angular-ui',
-    '<rootDir>/libs/angular-ui/user/types',
-    '<rootDir>/libs/angular-ui/auth/data',
-    '<rootDir>/libs/angular-ui/admin/data',
     '<rootDir>/libs/angular-ui/user/service',
     '<rootDir>/libs/angular-ui/auth/service',
   ],
