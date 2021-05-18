e1967c73b394c2743e87fd3f8310ecb8cffd1cf4
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-05-18 22:59:52 +0200

    angular-lib schematics

---
+++ tools/generators/angular-lib/files/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['<%= relativePathFromLibToRoot %>.eslintrc.js'],
+  ignorePatterns: ['!**/*'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: [
+        'plugin:@angular-eslint/recommended',
+        'plugin:@nrwl/nx/angular',
+        'plugin:@angular-eslint/template/process-inline-templates',
+      ],
+      parserOptions: { project: ['<%= libPath %>/tsconfig.*?.json'] },
+    },
+    {
+      files: ['*.html'],
+      extends: ['plugin:@nrwl/nx/angular-template', 'plugin:@angular-eslint/template/recommended'],
+      rules: {},
+    },
+    {
+      files: ['src/test-setup.ts'],
+      rules: { 'import/no-extraneous-dependencies': 'off' },
+    },
+  ],
+};
--- tools/generators/angular-lib/index.ts
+++ tools/generators/angular-lib/index.ts
@@ -1,9 +1,36 @@
-import { Tree } from '@nrwl/devkit';
+import {
+  formatFiles,
+  generateFiles,
+  installPackagesTask,
+  joinPathFragments,
+  Tree,
+} from '@nrwl/devkit';
+import * as path from 'path';
 import { wrapAngularDevkitSchematic } from '@nrwl/tao/src/commands/ngcli-adapter';
-import { Schema } from '@nrwl/tao/src/shared/params';
 
 export const libraryGenerator = wrapAngularDevkitSchematic('@nrwl/angular', 'library');
 
-export default async function (tree: Tree, options: Schema) {
+export default async function (tree: Tree, options: any) {
   await libraryGenerator(tree, options);
+  await formatFiles(tree);
+  deleteRootEslintFile(tree);
+  replaceEslintFileInLib(tree, options);
+  return () => {
+    installPackagesTask(tree);
+  };
+}
+
+function deleteRootEslintFile(tree: Tree) {
+  tree.delete('.eslintrc.json');
+}
+
+function replaceEslintFileInLib(tree: Tree, options: any) {
+  const libPath = `libs/${options.name}`;
+  const filesPath = joinPathFragments(__dirname, './files');
+  tree.delete(`${libPath}/.eslintrc.json`);
+  const substitutions = {
+    libPath,
+    relativePathFromLibToRoot: path.relative(path.dirname(libPath), path.dirname(tree.root)),
+  };
+  generateFiles(tree, filesPath, libPath, substitutions);
 }
