e1744abbd18c67b806c23445c407f293d619177a
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-05-19 11:15:08 +0200

    angular schematics cleaner index.ts

--- tools/generators/angular-lib/index.ts
+++ tools/generators/angular-lib/index.ts
@@ -1,12 +1,7 @@
-import {
-  formatFiles,
-  generateFiles,
-  installPackagesTask,
-  joinPathFragments,
-  Tree,
-} from '@nrwl/devkit';
+import { formatFiles, generateFiles, installPackagesTask, Tree } from '@nrwl/devkit';
 import * as path from 'path';
 import { wrapAngularDevkitSchematic } from '@nrwl/tao/src/commands/ngcli-adapter';
+import { stringUtils } from '@nrwl/workspace';
 
 export const libraryGenerator = wrapAngularDevkitSchematic('@nrwl/angular', 'library');
 
@@ -25,12 +20,12 @@ function deleteRootEslintFile(tree: Tree) {
 }
 
 function replaceEslintFileInLib(tree: Tree, options: any) {
-  const libPath = `libs/${options.name}`;
-  const filesPath = joinPathFragments(__dirname, './files');
-  tree.delete(`${libPath}/.eslintrc.json`);
+  const libPath = path.join('libs', stringUtils.dasherize(options.name));
+  const filesPath = path.join(__dirname, './files');
   const substitutions = {
     libPath,
     relativePathFromLibToRoot: path.relative(path.dirname(libPath), path.dirname(tree.root)),
   };
+  tree.delete(`${libPath}/.eslintrc.json`);
   generateFiles(tree, filesPath, libPath, substitutions);
 }
