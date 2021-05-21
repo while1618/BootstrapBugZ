f90c402a5e5d890c74f88e578091ff5365af0231
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-05-21 18:08:32 +0200

    angular app: organize imports

--- apps/angular-ui/src/app/app.module.ts
+++ apps/angular-ui/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
