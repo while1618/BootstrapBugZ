af0393ce94159f0f42818b66ccadbcf92bd44246
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-05-21 17:58:32 +0200

    removing service libs

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   projects: [
     '<rootDir>/apps/angular-ui',
-    '<rootDir>/libs/angular-ui/user/service',
-    '<rootDir>/libs/angular-ui/auth/service',
-    '<rootDir>/libs/angular-ui/admin/service',
     '<rootDir>/libs/shared',
     '<rootDir>/libs/angular-ui/shared',
   ],
--- libs/angular-ui/user/service/README.md
+++
@@ -1,7 +0,0 @@
-# angular-ui-user-service
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test angular-ui-user-service` to execute the unit tests.
