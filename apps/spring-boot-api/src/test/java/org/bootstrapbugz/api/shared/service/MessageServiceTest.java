1e9cad95bc64a7851f8c21cd9774bf0c9a37b607
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-06-15 01:47:52 +0200

    spring api: message service test

---
+++ apps/spring-boot-api/src/test/java/org/bootstrapbugz/api/shared/service/MessageServiceTest.java
@@ -0,0 +1,28 @@
+package org.bootstrapbugz.api.shared.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import org.bootstrapbugz.api.shared.message.service.impl.MessageServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+@ExtendWith(MockitoExtension.class)
+class MessageServiceTest {
+  @Mock private MessageSource messageSource;
+
+  @InjectMocks private MessageServiceImpl messageService;
+
+  @Test
+  void itShouldGetMessage() {
+    when(messageSource.getMessage("{token.invalid}", null, LocaleContextHolder.getLocale()))
+        .thenReturn("Invalid token.");
+    String actualMessage = messageService.getMessage("{token.invalid}");
+    assertThat(actualMessage).isEqualTo("Invalid token.");
+  }
+}
