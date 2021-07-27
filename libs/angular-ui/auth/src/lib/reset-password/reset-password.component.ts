0de76f3b206723d314dc1062475b2fc53e8856df
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-07-27 15:10:17 +0200

    angular ui: reset password validators

--- libs/angular-ui/auth/src/lib/reset-password/reset-password.component.ts
+++ libs/angular-ui/auth/src/lib/reset-password/reset-password.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { PASSWORD_REGEX } from '@bootstrapbugz/angular-ui/shared';
 import { Store } from '@ngrx/store';
 import { resetPassword } from '../+state/auth.actions';
 import { AuthState } from '../+state/auth.reducer';
@@ -13,13 +14,15 @@ import { ResetPasswordRequest } from '../auth.requests';
 })
 export class ResetPasswordComponent {
   resetPasswordForm = new FormGroup({
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.pattern(PASSWORD_REGEX)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.pattern(PASSWORD_REGEX)]),
   });
 
   constructor(private store: Store<AuthState>, private route: ActivatedRoute) {}
 
   onSubmit() {
+    if (!this.resetPasswordForm.valid) return;
+
     const request: ResetPasswordRequest = {
       token: this.route.snapshot.queryParamMap.get('token'),
       password: this.resetPasswordForm.controls.password.value,
