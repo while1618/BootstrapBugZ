5ffbe390d5580377e731a0ab48cc91e3b38f0c89
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-08-07 15:01:23 +0200

    format code

--- apps/angular-ui/src/app/app.module.ts
+++ apps/angular-ui/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
