b7a7870afc9c5a37cf9676ade1917fe7834102fc
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-10-10 16:56:15 +0200

    angular-ui fix compiler error in tests

--- apps/angular-ui/src/app/auth/validators/unique-email.spec.ts
+++ apps/angular-ui/src/app/auth/validators/unique-email.spec.ts
@@ -1,7 +1,16 @@
 import { UniqueEmail } from './unique-email';
+import { AuthService } from '../services/auth.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('UniqueEmail', () => {
+  let authService: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    authService = TestBed.inject(AuthService);
+  });
+
   it('should create an instance', () => {
-    expect(new UniqueEmail()).toBeTruthy();
+    expect(new UniqueEmail(authService)).toBeTruthy();
   });
 });
--- apps/angular-ui/src/app/auth/validators/unique-username.spec.ts
+++ apps/angular-ui/src/app/auth/validators/unique-username.spec.ts
@@ -1,7 +1,16 @@
 import { UniqueUsername } from './unique-username';
+import { TestBed } from '@angular/core/testing';
+import { AuthService } from '../services/auth.service';
 
 describe('UniqueUsername', () => {
+  let authService: AuthService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    authService = TestBed.inject(AuthService);
+  });
+
   it('should create an instance', () => {
-    expect(new UniqueUsername()).toBeTruthy();
+    expect(new UniqueUsername(authService)).toBeTruthy();
   });
 });
