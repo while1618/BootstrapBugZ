1ccec0f6874e1fa524759523c08eac6fa1a7f797
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2022-01-19 22:23:21 +0100

    angular-ui: settings component

--- apps/angular-ui/src/app/user/user-routing.module.ts
+++ apps/angular-ui/src/app/user/user-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { SettingsComponent } from './components/settings/settings.component';
 import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
@@ -9,6 +11,11 @@ const routes: Routes = [
       {
         path: ':username',
         component: UserComponent,
+      },
+      {
+        path: 'settings/profile',
+        component: SettingsComponent,
+        canActivate: [AuthGuard],
       },
     ],
   },
