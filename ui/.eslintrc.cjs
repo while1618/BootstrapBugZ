72b8d2ab735b024fc5a4386ebe6b1e9de00d52c0
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-04 14:41:34 +0100

    eslint and prettier

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -3,24 +3,37 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte'],
   },
   env: {
     es6: true,
     browser: true,
+    node: true,
   },
   ignorePatterns: ['.eslintrc.cjs'],
-  plugins: ['svelte3', '@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:svelte/recommended',
+    'plugin:svelte/prettier',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
   overrides: [
     {
       files: ['*.svelte'],
-      processor: 'svelte3/svelte3',
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
     },
   ],
-  settings: {
-    'svelte3/typescript': true,
-  },
   rules: {
-    '@typescript-eslint/no-inferrable-types': 'off',
+    'prettier/prettier': 'error',
+    'no-console': 'error',
   },
 };
