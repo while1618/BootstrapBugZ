2c4420f3fe9d4d9de7f3b8ca323897626b64feff
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-07 12:50:21 +0100

    forgot password form

--- svelte-kit/src/routes/auth/forgot-password/+page.svelte
+++ svelte-kit/src/routes/auth/forgot-password/+page.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
+  import { Button, Card, Input, Label } from 'flowbite-svelte';
 </script>
 
-<div class="flex flex-grow justify-center p-10">
-  <h1 class="text-lg dark:text-white">Forgot Password</h1>
+<div class="flex justify-center pt-10">
+  <Card size="sm" class="w-full">
+    <form class="flex flex-col space-y-6" action="/">
+      <h3 class="grid justify-items-center p-0 text-xl font-medium text-gray-900 dark:text-white">
+        Forgot password
+      </h3>
+      <Label class="space-y-2">
+        <span>Email</span>
+        <Input name="email" required />
+      </Label>
+      <Button type="submit" class="w-full">Send</Button>
+    </form>
+  </Card>
 </div>
 
 <style>
