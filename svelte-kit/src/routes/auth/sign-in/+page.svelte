01512261be4606dfdcee2a2d842a9b88af35676d
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-07 03:39:17 +0100

    Sign in form

--- svelte-kit/src/routes/auth/sign-in/+page.svelte
+++ svelte-kit/src/routes/auth/sign-in/+page.svelte
@@ -1,8 +1,34 @@
 <script lang="ts">
+  import { Button, Card, Input, Label } from 'flowbite-svelte';
 </script>
 
-<div class="flex flex-grow justify-center p-10">
-  <h1 class="text-lg dark:text-white">Sign In</h1>
+<div class="flex justify-center pt-10">
+  <Card size="sm" class="w-full">
+    <form class="flex flex-col space-y-6" action="/">
+      <h3 class="grid justify-items-center p-0 text-xl font-medium text-gray-900 dark:text-white">
+        Sign in to BootstrapBugZ
+      </h3>
+      <Label class="space-y-2">
+        <span>Username or Email</span>
+        <Input type="email" name="email" required />
+      </Label>
+      <Label class="space-y-2">
+        <div class="flex justify-between">
+          <span>Password</span>
+          <a href="/" class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
+            >Forgot password?</a
+          >
+        </div>
+        <Input type="password" name="password" required />
+      </Label>
+      <Button type="submit" class="w-full">Sign in</Button>
+      <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
+        Not registered? <a href="/" class="text-blue-700 hover:underline dark:text-blue-500"
+          >Create an account</a
+        >
+      </div>
+    </form>
+  </Card>
 </div>
 
 <style>
