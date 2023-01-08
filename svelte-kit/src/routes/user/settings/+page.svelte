551b0d53af293c97474b159d68b94e2e4ea8b41a
Dejan Zdravkovic <bagzi1996@gmail.com>
2023-01-08 01:22:16 +0100

    settings form

--- svelte-kit/src/routes/user/settings/+page.svelte
+++ svelte-kit/src/routes/user/settings/+page.svelte
@@ -6,69 +6,75 @@
   <Card
     size="md"
     color="none"
-    class="w-full border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800"
+    class="border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800"
   >
     <Tabs style="underline" class="justify-center">
       <TabItem open title="Update profile">
-        <form class="flex flex-col space-y-6" action="/">
-          <FloatingLabelInput
-            class="space-y-2"
-            style="standard"
-            id="firstName"
-            name="firstName"
-            type="text"
-            label="First name"
-          />
-          <FloatingLabelInput
-            class="space-y-2"
-            style="standard"
-            id="lastName"
-            name="lastName"
-            type="text"
-            label="Last name"
-          />
-          <FloatingLabelInput
-            class="space-y-2"
-            style="standard"
-            id="username"
-            name="username"
-            type="text"
-            label="Username"
-          />
-          <FloatingLabelInput
-            class="space-y-2"
-            style="standard"
-            id="email"
-            name="email"
-            type="email"
-            label="Email"
-          />
-          <Button type="submit" class="w-full">Update</Button>
-        </form>
+        <div class="flex justify-center">
+          <form class="flex w-2/3 flex-col space-y-6" action="/">
+            <FloatingLabelInput
+              class="space-y-2"
+              style="standard"
+              id="firstName"
+              name="firstName"
+              type="text"
+              label="First name"
+            />
+            <FloatingLabelInput
+              class="space-y-2"
+              style="standard"
+              id="lastName"
+              name="lastName"
+              type="text"
+              label="Last name"
+            />
+            <FloatingLabelInput
+              class="space-y-2"
+              style="standard"
+              id="username"
+              name="username"
+              type="text"
+              label="Username"
+            />
+            <FloatingLabelInput
+              class="space-y-2"
+              style="standard"
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+            />
+            <Button type="submit">Update</Button>
+          </form>
+        </div>
       </TabItem>
       <TabItem title="Change password">
-        <form class="flex flex-col space-y-6" action="/">
-          <FloatingLabelInput
-            class="space-y-2"
-            style="standard"
-            id="password"
-            name="password"
-            type="password"
-            label="Password"
-          />
-          <FloatingLabelInput
-            class="space-y-2"
-            style="standard"
-            id="confirmPassword"
-            name="confirmPassword"
-            type="password"
-            label="Confirm password"
-          />
-          <Button type="submit">Change password</Button>
-        </form>
+        <div class="flex justify-center">
+          <form class="flex w-2/3 flex-col space-y-6" action="/">
+            <FloatingLabelInput
+              class="space-y-2"
+              style="standard"
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+            />
+            <FloatingLabelInput
+              class="space-y-2"
+              style="standard"
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              label="Confirm password"
+            />
+            <Button type="submit">Change password</Button>
+          </form>
+        </div>
       </TabItem>
       <TabItem title="Logged in devices">
-        <Button type="submit" class="w-full">Sign out from all devices</Button>
+        <div class="flex justify-center">
+          <Button type="submit" class="w-2/3">Sign out from all devices</Button>
+        </div>
       </TabItem>
     </Tabs>
   </Card>
