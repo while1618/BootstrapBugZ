ac6af140e6164d213ad631a01cc9107f0b99413f
Dejan Zdravkovic <bagzi1996@gmail.com>
2023-01-08 02:46:59 +0100

    add card to root and profile

--- svelte-kit/src/routes/+page.svelte
+++ svelte-kit/src/routes/+page.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
+  import { Card } from 'flowbite-svelte';
 </script>
 
-<div class="flex flex-grow justify-center p-10">
-  <h1 class="text-lg dark:text-white">BootstrapBugZ</h1>
+<div class="flex justify-center pt-10">
+  <Card
+    size="sm"
+    color="none"
+    class="w-full border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800"
+  >
+    <div class="flex justify-center">
+      <h1 class="text-lg dark:text-white">BootstrapBugZ</h1>
+    </div>
+  </Card>
 </div>
 
 <style>
--- svelte-kit/src/routes/user/[name]/+page.svelte
+++ svelte-kit/src/routes/user/[name]/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
+  import { Card } from 'flowbite-svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
 </script>
 
-<div class="flex flex-grow justify-center p-10">
-  <h1 class="text-lg dark:text-white">{data.name}</h1>
+<div class="flex justify-center pt-10">
+  <Card
+    size="sm"
+    color="none"
+    class="w-full border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800"
+  >
+    <div class="flex justify-center">
+      <h1 class="text-lg dark:text-white">{data.name}</h1>
+    </div>
+  </Card>
 </div>
 
 <style>
