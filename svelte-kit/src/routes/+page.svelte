de6034a34fd9e6ec129be4951350ecf269ed6a13
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-09 02:49:55 +0100

    switch navbar

--- svelte-kit/src/routes/+page.svelte
+++ svelte-kit/src/routes/+page.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
   import NoAuthNavbar from '$lib/navbar/NoAuthNavbar.svelte';
-  import { Card } from 'flowbite-svelte';
+  import UserNavbar from '$lib/navbar/UserNavbar.svelte';
+  import { Button, Card } from 'flowbite-svelte';
+
+  let signedIn = false;
 </script>
 
-<NoAuthNavbar />
+{#if signedIn}
+  <UserNavbar />
+{:else}
+  <NoAuthNavbar />
+{/if}
+
 <div class="flex justify-center pt-10">
   <Card
     size="sm"
     color="none"
-    class="w-full border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800"
+    class="w-full gap-5 border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800"
   >
     <div class="flex justify-center">
       <h1 class="text-lg dark:text-white">BootstrapBugZ</h1>
     </div>
+    <Button on:click={() => (signedIn = !signedIn)}>Switch Navbar</Button>
   </Card>
 </div>
 
