7ffd172a3d310d1cdb1c514678e017c3f3101c34
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-09 23:07:46 +0100

    icons

--- svelte-kit/src/lib/navbar/UserNavbar.svelte
+++ svelte-kit/src/lib/navbar/UserNavbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
+  import avatar from '$lib/images/avatar.svg';
   import logo from '$lib/images/BootstrapBugZ.png';
-  import github from '$lib/images/github-mark.svg';
   import {
     Avatar,
     CloseButton,
@@ -36,7 +36,7 @@
     </span>
   </NavBrand>
   <div class="flex md:gap-2">
-    <Avatar class="hidden md:flex" id="avatar-menu" src={github} />
+    <Avatar class="hidden md:flex" id="avatar-menu" src={avatar} />
     <Dropdown placement="bottom" triggeredBy="#avatar-menu">
       <DropdownHeader>
         <a href="/" class="block text-sm">@while1618</a>
@@ -56,14 +56,87 @@
       <Sidebar>
         <SidebarWrapper>
           <SidebarGroup>
-            <SidebarItem label="@while1618" href="/" />
+            <SidebarItem label="@while1618" href="/">
+              <svelte:fragment slot="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-6 w-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </svelte:fragment>
+            </SidebarItem>
           </SidebarGroup>
           <SidebarGroup border>
-            <SidebarItem label="Dashboard" href="/" />
-            <SidebarItem label="Settings" href="/user/settings" />
+            <SidebarItem label="Dashboard" href="/">
+              <svelte:fragment slot="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-6 w-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z"
+                  />
+                </svg>
+              </svelte:fragment>
+            </SidebarItem>
+            <SidebarItem label="Settings" href="/user/settings">
+              <svelte:fragment slot="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-6 w-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </svelte:fragment>
+            </SidebarItem>
           </SidebarGroup>
           <SidebarGroup border>
-            <SidebarItem label="Sign out" href="/" />
+            <SidebarItem label="Sign out" href="/">
+              <svelte:fragment slot="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-6 w-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                  />
+                </svg>
+              </svelte:fragment>
+            </SidebarItem>
           </SidebarGroup>
         </SidebarWrapper>
       </Sidebar>
