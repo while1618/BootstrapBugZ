37d25d9dc120acb8303a4532107983839b2fbd78
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-10 23:49:59 +0100

    theme

--- svelte-kit/src/routes/+layout.svelte
+++ svelte-kit/src/routes/+layout.svelte
@@ -2,6 +2,6 @@
   import '../app.css';
 </script>
 
-<div class="flex min-h-screen flex-col bg-gray-50 dark:bg-gray-900">
+<div class="flex min-h-screen flex-col bg-gray-50 dark:bg-gray-800">
   <slot />
 </div>
