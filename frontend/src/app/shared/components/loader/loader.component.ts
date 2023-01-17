db8e26a4d447ebd331b3782696ddcdcafc214a6e
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-17 23:01:56 +0100

    Delete old angular app

--- frontend/src/app/shared/components/loader/loader.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'bootstrapbugz-loader',
-  templateUrl: './loader.component.html',
-  styleUrls: ['./loader.component.scss'],
-})
-export class LoaderComponent {}
--- frontend/src/app/shared/error/handling/custom-error-state-matcher.ts
+++
@@ -1,9 +0,0 @@
-import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-
-export class CustomErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl, form: FormGroupDirective | NgForm): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
