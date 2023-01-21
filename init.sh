28f658bccfbb849b4220fcf2c3026ffbc1f97d61
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-21 18:29:45 +0100

    init

--- init.sh
+++ init.sh
@@ -11,5 +11,14 @@ echo "Pick your frontend: "
 FRONEND=$(gum choose "svelte-kit")
 clear
 
+find ./backend -mindepth 1 ! -regex "^./backend/$BACKEND\(/.*\)?" -delete
+find ./frontend -mindepth 1 ! -regex "^./frontend/$FRONEND\(/.*\)?" -delete
+
+cp -r ./backend/$BACKEND/. ./backend
+cp -r ./frontend/$FRONEND/. ./frontend
+
+rm -rf ./backend/$BACKEND
+rm -rf ./frontend/$FRONEND
+
 echo "Name of your app is: $APP_NAME"
 echo "You picked $BACKEND for backend, and $FRONEND for frontend."
