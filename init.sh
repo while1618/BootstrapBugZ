6bb2c4a9f1b7013b760c2e7e42cb623555c93500
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-21 17:14:57 +0100

    init script

--- init.sh
+++ init.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+clear
+echo "Name of your app: "
+APP_NAME=$(gum input --placeholder "")
+clear
+echo "Pick your backend: "
+BACKEND=$(gum choose "spring-boot")
+clear
 echo "Pick your frontend: "
 FRONEND=$(gum choose "svelte-kit")
-echo "Pick your backend: "
-BACKED=$(gum choose "spring-boot")
+clear
 
-echo "You picked $FRONEND for frontend, and $BACKED for backend."
+echo "Name of your app is: $APP_NAME"
+echo "You picked $BACKEND for backend, and $FRONEND for frontend."
