dbaf3de9f39f216b26146cf8ae2f83ae9b063ad2
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-01-30 00:34:27 +0100

    resetPassword form

--- frontend/svelte-kit/src/routes/auth/reset-password/+page.server.ts
+++ frontend/svelte-kit/src/routes/auth/reset-password/+page.server.ts
@@ -1,14 +1,32 @@
 import { API_URL } from '$lib/apis/api';
+import en from '$lib/i18n/en.json';
 import type { ErrorMessage } from '$lib/models/error-message';
+import { PASSWORD_REGEX } from '$lib/regex/regex';
 import { fail, redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
+
+interface ResetPasswordRequest {
+  accessToken: string;
+  password: string;
+  confirmPassword: string;
+}
+
+interface ResetPasswordErrors {
+  accessToken: string | null;
+  password: string | null;
+  confirmPassword: string | null;
+}
 
 export const actions = {
   resetPassword: async ({ fetch, request }) => {
     const formData = await request.formData();
+    const resetPasswordRequest = getResetPasswordRequest(formData);
+    const errors = checkResetPasswordRequest(resetPasswordRequest);
+    if (!isObjectEmpty(errors)) return fail(400, { errors });
+
     const response = await fetch(`${API_URL}/auth/reset-password`, {
       method: 'PUT',
-      body: JSON.stringify(resetPasswordRequest(formData)),
+      body: JSON.stringify(resetPasswordRequest),
       headers: {
         'Content-Type': 'application/json',
       },
@@ -16,6 +34,7 @@ export const actions = {
 
     if (response.status !== 204) {
       const errorMessage = (await response.json()) as ErrorMessage;
+      console.log(errorMessage);
       return fail(response.status, { errorMessage });
     }
 
@@ -23,9 +42,32 @@ export const actions = {
   },
 } satisfies Actions;
 
-const resetPasswordRequest = (request: FormData) => {
+const getResetPasswordRequest = (request: FormData): ResetPasswordRequest => {
   return {
+    accessToken: '',
     password: request.get('password'),
     confirmPassword: request.get('confirmPassword'),
+  } as ResetPasswordRequest;
+};
+
+const checkResetPasswordRequest = (request: ResetPasswordRequest): ResetPasswordErrors => {
+  const errors: ResetPasswordErrors = {
+    accessToken: null,
+    password: null,
+    confirmPassword: null,
   };
+
+  if (request.accessToken === '') errors.accessToken = en['token.invalid'];
+  if (!PASSWORD_REGEX.test(request.password)) errors.password = en['password.invalid'];
+  if (!PASSWORD_REGEX.test(request.confirmPassword))
+    errors.confirmPassword = en['password.invalid'];
+  if (request.password !== request.confirmPassword)
+    errors.confirmPassword = en['password.doNotMatch'];
+
+  return errors;
+};
+
+const isObjectEmpty = (obj: object): boolean => {
+  const values = Object.values(obj);
+  return values.every((val) => val === null);
 };
--- frontend/svelte-kit/src/routes/auth/reset-password/+page.svelte
+++ frontend/svelte-kit/src/routes/auth/reset-password/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
   import { Button, Card, FloatingLabelInput } from 'flowbite-svelte';
+  import type { ActionData } from './$types';
+
+  export let form: ActionData;
 </script>
 
 <div class="flex justify-center pt-10">
@@ -9,26 +12,44 @@
     color="none"
     class="w-full border-gray-100 bg-white dark:border-gray-700 dark:bg-gray-800"
   >
-    <form class="flex flex-col space-y-6" method="PUT" action="?/resetPassword" use:enhance>
+    <form class="flex flex-col space-y-6" method="POST" action="?/resetPassword" use:enhance>
       <h3 class="grid justify-items-center p-0 text-xl font-medium text-gray-900 dark:text-white">
         Reset password
       </h3>
-      <FloatingLabelInput
-        class="space-y-2"
-        style="standard"
-        id="password"
-        name="password"
-        type="password"
-        label="Password"
-      />
-      <FloatingLabelInput
-        class="space-y-2"
-        style="standard"
-        id="confirmPassword"
-        name="confirmPassword"
-        type="password"
-        label="Confirm password"
-      />
+      <div>
+        <FloatingLabelInput
+          class="space-y-2"
+          style="standard"
+          id="password"
+          name="password"
+          type="password"
+          label="Password"
+        />
+        {#if form?.errors?.password}
+          <p class="mt-2 text-sm text-red-600">{form.errors.password}</p>
+        {/if}
+      </div>
+      <div>
+        <FloatingLabelInput
+          class="space-y-2"
+          style="standard"
+          id="confirmPassword"
+          name="confirmPassword"
+          type="password"
+          label="Confirm password"
+        />
+        {#if form?.errors?.confirmPassword}
+          <p class="mt-2 text-sm text-red-600">{form.errors.confirmPassword}</p>
+        {/if}
+      </div>
+      {#if form?.errors?.accessToken}
+        <p class="mt-2 text-sm text-red-600">{form.errors.accessToken}</p>
+      {/if}
+      {#if form?.errorMessage}
+        {#each form.errorMessage.details as error}
+          <p class="mt-2 text-sm text-red-600">{error.message}</p>
+        {/each}
+      {/if}
       <Button type="submit">Reset password</Button>
     </form>
   </Card>
