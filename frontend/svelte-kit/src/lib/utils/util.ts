833bcb8a9d88a5998c22e56a65836e3bea90746c
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-02-05 19:20:20 +0100

    decodeJWT

--- frontend/svelte-kit/src/lib/utils/util.ts
+++ frontend/svelte-kit/src/lib/utils/util.ts
@@ -2,3 +2,7 @@ export function isObjectEmpty(obj: object): boolean {
   const values = Object.values(obj);
   return values.every((val) => val === null);
 }
+
+export function decodeJWT(jwt: string): object {
+  return JSON.parse(Buffer.from(jwt.split('.')[1], 'base64').toString());
+}
