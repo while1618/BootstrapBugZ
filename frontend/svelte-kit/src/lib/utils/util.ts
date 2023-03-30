c3185694226964d73f5734018dd6fa0802877bf5
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-03-31 01:14:44 +0200

    cleanup

--- frontend/svelte-kit/src/lib/utils/util.ts
+++ frontend/svelte-kit/src/lib/utils/util.ts
@@ -1,10 +1,5 @@
 import type { JwtPayload } from '$lib/models/jwt-payload';
 
-export function isObjectEmpty(obj: object): boolean {
-  const values = Object.values(obj);
-  return values.every((val) => val === null);
-}
-
 export function decodeJWT(jwt: string): JwtPayload {
   return JSON.parse(Buffer.from(jwt.split('.')[1], 'base64').toString()) as JwtPayload;
 }
