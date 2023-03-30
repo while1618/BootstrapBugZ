1694f93b8217af5b434cdda394e947c4b900ebbf
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-03-30 22:47:37 +0200

    forgotPassword form validation with zod

--- frontend/svelte-kit/src/routes/auth/forgot-password/+page.server.ts
+++ frontend/svelte-kit/src/routes/auth/forgot-password/+page.server.ts
@@ -1,17 +1,13 @@
 import { HttpRequest, makeRequest } from '$lib/apis/api';
 import en from '$lib/i18n/en.json';
 import { EMAIL_REGEX } from '$lib/regex/regex';
-import { isObjectEmpty } from '$lib/utils/util';
 import { fail, redirect } from '@sveltejs/kit';
+import { z } from 'zod';
 import type { Actions, PageServerLoad } from './$types';
 
-interface ForgotPasswordRequest {
-  email: string;
-}
-
-interface ForgotPasswordErrors {
-  email: string | null;
-}
+const forgotPasswordSchema = z.object({
+  email: z.string().regex(EMAIL_REGEX, { message: en['email.invalid'] }),
+});
 
 export const load = (({ locals }) => {
   if (locals.user) throw redirect(302, '/');
@@ -19,15 +15,15 @@ export const load = (({ locals }) => {
 
 export const actions = {
   forgotPassword: async ({ request }) => {
-    const formData = await request.formData();
-    const forgotPasswordRequest = getForgotPasswordRequest(formData);
-    const errors = await checkForgotPasswordRequest(forgotPasswordRequest);
-    if (!isObjectEmpty(errors)) return fail(400, { errors });
+    const formData = Object.fromEntries(await request.formData());
+    const forgotPasswordForm = forgotPasswordSchema.safeParse(formData);
+    if (!forgotPasswordForm.success)
+      return fail(400, { errors: forgotPasswordForm.error.flatten().fieldErrors });
 
     const response = await makeRequest({
       method: HttpRequest.POST,
       path: '/auth/forgot-password',
-      body: JSON.stringify(forgotPasswordRequest),
+      body: JSON.stringify(forgotPasswordForm.data),
     });
 
     if ('error' in response) return fail(response.status, { errorMessage: response });
@@ -35,22 +31,3 @@ export const actions = {
     throw redirect(303, '/auth/sign-in');
   },
 } satisfies Actions;
-
-const getForgotPasswordRequest = (request: FormData): ForgotPasswordRequest => {
-  return {
-    email: request.get('email'),
-  } as ForgotPasswordRequest;
-};
-
-const checkForgotPasswordRequest = async (
-  request: ForgotPasswordRequest
-): Promise<ForgotPasswordErrors> => {
-  const errors: ForgotPasswordErrors = {
-    email: null,
-  };
-
-  if (request.email === '') errors.email = en['email.invalid'];
-  if (!EMAIL_REGEX.test(request.email)) errors.email = en['email.invalid'];
-
-  return errors;
-};
