8c8d62dfbffedc4c478853adf7412c8fecad6eed
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-04-10 02:09:38 +0200

    fix load function

--- frontend/svelte-kit/src/routes/user/[name]/+page.server.ts
+++ frontend/svelte-kit/src/routes/user/[name]/+page.server.ts
@@ -1,5 +1,6 @@
 import { HttpRequest, makeRequest } from '$lib/apis/api';
 import type { UserDTO } from '$lib/models/user';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ params, cookies }) => {
@@ -9,7 +10,7 @@ export const load = (async ({ params, cookies }) => {
     auth: cookies.get('accessToken'),
   });
 
-  if ('error' in response) return new Response(String(response.error));
+  if ('error' in response) throw error(response.status, response.details[0].message);
 
   return { user: response as UserDTO };
 }) satisfies PageServerLoad;
