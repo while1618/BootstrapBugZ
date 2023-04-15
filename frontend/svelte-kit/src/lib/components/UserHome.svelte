a9fc37f9cc1171b79610a3a9d5af6359d9c55d32
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-04-15 20:19:25 +0200

    userHome

--- frontend/svelte-kit/src/lib/components/UserHome.svelte
+++ frontend/svelte-kit/src/lib/components/UserHome.svelte
@@ -5,7 +5,9 @@
 <div class="hero min-h-screen">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <h1 class="text-5xl font-bold">{$userStore?.username}</h1>
+      {#if $userStore}
+        <h1 class="text-5xl font-bold">{$userStore.username}</h1>
+      {/if}
     </div>
   </div>
 </div>
