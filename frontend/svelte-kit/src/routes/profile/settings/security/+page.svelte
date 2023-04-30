ecaa3ef459432d9dd906d4ec005c4359aa7890c8
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-04-30 20:45:20 +0200

    divider

--- frontend/svelte-kit/src/routes/profile/settings/security/+page.svelte
+++ frontend/svelte-kit/src/routes/profile/settings/security/+page.svelte
@@ -8,71 +8,78 @@
 <div class="hero min-h-screen">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <form class="flex flex-col gap-4" method="POST" action="?/changePassword" use:enhance>
-        <div class="form-control w-full max-w-xs">
-          <label for="oldPassword" class="label">
-            <span class="label-text">Old password</span>
-          </label>
-          <input
-            type="password"
-            id="oldPassword"
-            name="oldPassword"
-            class="input-bordered input w-full max-w-xs"
-          />
-          {#if form?.errors?.oldPassword}
-            <label for="oldPassword" class="label">
-              <span class="label-text text-error">{form.errors.oldPassword[0]}</span>
-            </label>
-          {/if}
-        </div>
-
-        <div class="form-control w-full max-w-xs">
-          <label for="newPassword" class="label">
-            <span class="label-text">New password</span>
-          </label>
-          <input
-            type="password"
-            id="newPassword"
-            name="newPassword"
-            class="input-bordered input w-full max-w-xs"
-          />
-          {#if form?.errors?.newPassword}
-            <label for="newPassword" class="label">
-              <span class="label-text text-error">{form.errors.newPassword[0]}</span>
-            </label>
-          {/if}
-        </div>
-
-        <div class="form-control w-full max-w-xs">
-          <label for="confirmNewPassword" class="label">
-            <span class="label-text">Confirm new password</span>
-          </label>
-          <input
-            type="password"
-            id="confirmNewPassword"
-            name="confirmNewPassword"
-            class="input-bordered input w-full max-w-xs"
-          />
-          {#if form?.errors?.confirmNewPassword}
-            <label for="confirmNewPassword" class="label">
-              <span class="label-text text-error">{form.errors.confirmNewPassword[0]}</span>
-            </label>
-          {/if}
-        </div>
-
-        {#if form?.errorMessage}
-          {#each form.errorMessage.details as error}
-            <div class="flex">
-              <p class="label-text text-error">{error.message}</p>
+      <div class="flex w-full flex-col">
+        <div class="card">
+          <form class="flex flex-col gap-4" method="POST" action="?/changePassword" use:enhance>
+            <div class="form-control w-full max-w-xs">
+              <label for="oldPassword" class="label">
+                <span class="label-text">Old password</span>
+              </label>
+              <input
+                type="password"
+                id="oldPassword"
+                name="oldPassword"
+                class="input-bordered input w-full max-w-xs"
+              />
+              {#if form?.errors?.oldPassword}
+                <label for="oldPassword" class="label">
+                  <span class="label-text text-error">{form.errors.oldPassword[0]}</span>
+                </label>
+              {/if}
             </div>
-          {/each}
-        {/if}
 
-        <button class="btn-primary btn">Change password</button>
-        <a href="/auth/sign-out-from-all-devices" class="btn-primary btn">
-          Sign out form all devices
-        </a>
-      </form>
+            <div class="form-control w-full max-w-xs">
+              <label for="newPassword" class="label">
+                <span class="label-text">New password</span>
+              </label>
+              <input
+                type="password"
+                id="newPassword"
+                name="newPassword"
+                class="input-bordered input w-full max-w-xs"
+              />
+              {#if form?.errors?.newPassword}
+                <label for="newPassword" class="label">
+                  <span class="label-text text-error">{form.errors.newPassword[0]}</span>
+                </label>
+              {/if}
+            </div>
+
+            <div class="form-control w-full max-w-xs">
+              <label for="confirmNewPassword" class="label">
+                <span class="label-text">Confirm new password</span>
+              </label>
+              <input
+                type="password"
+                id="confirmNewPassword"
+                name="confirmNewPassword"
+                class="input-bordered input w-full max-w-xs"
+              />
+              {#if form?.errors?.confirmNewPassword}
+                <label for="confirmNewPassword" class="label">
+                  <span class="label-text text-error">{form.errors.confirmNewPassword[0]}</span>
+                </label>
+              {/if}
+            </div>
+
+            {#if form?.errorMessage}
+              {#each form.errorMessage.details as error}
+                <div class="flex">
+                  <p class="label-text text-error">{error.message}</p>
+                </div>
+              {/each}
+            {/if}
+
+            <button class="btn-primary btn">Change password</button>
+          </form>
+        </div>
+        <div class="divider" />
+        <div class="card">
+          <a href="/auth/sign-out-from-all-devices" class="btn-primary btn">
+            Sign out form all devices
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </div>
