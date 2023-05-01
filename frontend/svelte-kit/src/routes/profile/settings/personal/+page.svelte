7650f969a3c97ec810795a8488f61caf4b525f9c
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-05-01 03:52:02 +0200

    personal info value populated

--- frontend/svelte-kit/src/routes/profile/settings/personal/+page.svelte
+++ frontend/svelte-kit/src/routes/profile/settings/personal/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
+  import { userStore } from '$lib/stores/user';
   import type { ActionData } from '../personal/$types';
 
   export let form: ActionData;
@@ -8,7 +9,16 @@
 <div class="hero min-h-screen">
   <div class="hero-content text-center">
     <div class="max-w-md">
-      <form class="flex flex-col gap-4" method="POST" action="?/updateProfile" use:enhance>
+      <form
+        class="flex flex-col gap-4"
+        method="POST"
+        action="?/updateProfile"
+        use:enhance={() => {
+          return async ({ update }) => {
+            update({ reset: false });
+          };
+        }}
+      >
         <div class="form-control w-full max-w-xs">
           <label for="firstName" class="label">
             <span class="label-text">First name</span>
@@ -18,6 +28,7 @@
             id="firstName"
             name="firstName"
             class="input-bordered input w-full max-w-xs"
+            value={$userStore?.firstName}
           />
           {#if form?.errors?.firstName}
             <label for="firstName" class="label">
@@ -35,6 +46,7 @@
             id="lastName"
             name="lastName"
             class="input-bordered input w-full max-w-xs"
+            value={$userStore?.lastName}
           />
           {#if form?.errors?.lastName}
             <label for="lastName" class="label">
@@ -52,6 +64,7 @@
             id="username"
             name="username"
             class="input-bordered input w-full max-w-xs"
+            value={$userStore?.username}
           />
           {#if form?.errors?.username}
             <label for="username" class="label">
@@ -69,6 +82,7 @@
             id="email"
             name="email"
             class="input-bordered input w-full max-w-xs"
+            value={$userStore?.email}
           />
           {#if form?.errors?.email}
             <label for="email" class="label">
