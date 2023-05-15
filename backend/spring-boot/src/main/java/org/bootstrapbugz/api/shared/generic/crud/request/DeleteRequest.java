c45d366052d97519be1daee5be0eb32529cb8e28
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-05-15 19:19:49 +0200

    DeleteRequest record

--- backend/spring-boot/src/main/java/org/bootstrapbugz/api/shared/generic/crud/request/DeleteRequest.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/api/shared/generic/crud/request/DeleteRequest.java
@@ -2,16 +2,5 @@ package org.bootstrapbugz.api.shared.generic.crud.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import java.util.Set;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class DeleteRequest {
-  @NotEmpty(message = "{ids.empty}")
-  private Set<Long> ids;
-}
+public record DeleteRequest(@NotEmpty(message = "{ids.empty}") Set<Long> ids) {}
