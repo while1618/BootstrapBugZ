ddccf5768e09f153bc760974132ed27b18c465c2
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-05-28 17:08:50 +0200

    remove unnecessary method from IntegrationTestUtil

--- backend/spring-boot/src/test/java/org/bootstrapbugz/api/shared/util/IntegrationTestUtil.java
+++ backend/spring-boot/src/test/java/org/bootstrapbugz/api/shared/util/IntegrationTestUtil.java
@@ -1,6 +1,5 @@
 package org.bootstrapbugz.api.shared.util;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,27 +8,11 @@ import org.bootstrapbugz.api.auth.jwt.util.JwtUtil;
 import org.bootstrapbugz.api.auth.payload.dto.SignInDTO;
 import org.bootstrapbugz.api.auth.payload.request.SignInRequest;
 import org.bootstrapbugz.api.shared.constants.Path;
-import org.bootstrapbugz.api.shared.error.ErrorMessage;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 public class IntegrationTestUtil {
   private IntegrationTestUtil() {}
-
-  public static void checkErrorMessages(ErrorMessage expectedResponse, ResultActions resultActions)
-      throws Exception {
-    final var actualResponse =
-        new JSONObject(resultActions.andReturn().getResponse().getContentAsString());
-    assertThat(actualResponse.getInt("status")).isEqualTo(expectedResponse.getStatus());
-    assertThat(actualResponse.getString("error")).isEqualTo(expectedResponse.getError());
-    assertThat(actualResponse.getJSONArray("details"))
-        .usingRecursiveComparison()
-        .ignoringCollectionOrder()
-        .isEqualTo(new JSONArray(expectedResponse.getDetails().toString()));
-  }
 
   public static SignInDTO signIn(MockMvc mockMvc, ObjectMapper objectMapper, String username)
       throws Exception {
