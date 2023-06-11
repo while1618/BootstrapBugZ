b4a58331aef3b6d39171dcb3c8faa73511e00d08
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-06-11 16:08:51 +0200

    admin patch and update user

--- backend/spring-boot/src/main/java/org/bootstrapbugz/api/admin/service/impl/UserServiceImpl.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/api/admin/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package org.bootstrapbugz.api.admin.service.impl;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import org.bootstrapbugz.api.admin.payload.request.PatchUserRequest;
 import org.bootstrapbugz.api.admin.payload.request.UserRequest;
@@ -83,10 +84,10 @@ public class UserServiceImpl implements UserService {
     final var user = userRepository.findByIdWithRoles(id).orElse(User.builder().id(id).build());
 
     if (userRepository.existsByUsername(userRequest.username())
-        && !user.getUsername().equals(userRequest.username()))
+        && Objects.equals(user.getUsername(), userRequest.username()))
       throw new ConflictException(messageService.getMessage("username.exists"));
     if (userRepository.existsByEmail(userRequest.email())
-        && !user.getEmail().equals(userRequest.email()))
+        && Objects.equals(user.getEmail(), userRequest.email()))
       throw new ConflictException(messageService.getMessage("email.exists"));
 
     final var roles = Set.copyOf(roleRepository.findAllByNameIn(userRequest.roleNames()));
@@ -99,7 +100,10 @@ public class UserServiceImpl implements UserService {
     user.setLock(userRequest.lock());
     user.setRoles(roles);
 
-    if (!userRequest.active() || userRequest.lock() || !roles.equals(user.getRoles())) {
+    if (!bCryptPasswordEncoder.matches(userRequest.password(), user.getPassword())
+        || !userRequest.active()
+        || userRequest.lock()
+        || !roles.equals(user.getRoles())) {
       accessTokenService.invalidateAllByUserId(id);
       refreshTokenService.deleteAllByUserId(id);
     }
@@ -109,7 +113,50 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public UserDTO patch(Long id, PatchUserRequest patchUserRequest) {
-    return null;
+    final var user =
+        userRepository
+            .findByIdWithRoles(id)
+            .orElseThrow(
+                () -> new ResourceNotFoundException(messageService.getMessage("user.notFound")));
+
+    final var roles = Set.copyOf(roleRepository.findAllByNameIn(patchUserRequest.roleNames()));
+    if (patchUserRequest.firstName() != null) user.setFirstName(patchUserRequest.firstName());
+    if (patchUserRequest.lastName() != null) user.setLastName(patchUserRequest.lastName());
+    tryToSetUsername(user, patchUserRequest.username());
+    tryToSetEmail(user, patchUserRequest.email());
+    if (patchUserRequest.password() != null)
+      user.setPassword(bCryptPasswordEncoder.encode(patchUserRequest.password()));
+    if (patchUserRequest.active() != null) user.setActive(patchUserRequest.active());
+    if (patchUserRequest.lock() != null) user.setLock(patchUserRequest.lock());
+    if (patchUserRequest.roleNames() != null) user.setRoles(roles);
+
+    if (!bCryptPasswordEncoder.matches(patchUserRequest.password(), user.getPassword())
+        || Boolean.FALSE.equals(patchUserRequest.active())
+        || Boolean.TRUE.equals(patchUserRequest.lock())
+        || (patchUserRequest.roleNames() != null && !roles.equals(user.getRoles()))) {
+      accessTokenService.invalidateAllByUserId(id);
+      refreshTokenService.deleteAllByUserId(id);
+    }
+
+    return UserMapper.INSTANCE.userToAdminUserDTO(userRepository.save(user));
+  }
+
+  private void tryToSetUsername(User user, String username) {
+    if (username == null) return;
+    if (user.getUsername().equals(username)) return;
+    if (userRepository.existsByUsername(username))
+      throw new ConflictException("username", messageService.getMessage("username.exists"));
+
+    user.setUsername(username);
+  }
+
+  private void tryToSetEmail(User user, String email) {
+    if (email == null) return;
+    if (user.getEmail().equals(email)) return;
+    if (userRepository.existsByEmail(email))
+      throw new ConflictException("email", messageService.getMessage("email.exists"));
+
+    user.setEmail(email);
   }
 
   @Override
