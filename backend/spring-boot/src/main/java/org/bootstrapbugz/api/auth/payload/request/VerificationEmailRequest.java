58f5b5a4de94358adbe11f40546da6de969f7054
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2023-06-11 03:23:42 +0200

    VerificationEmailRequest

--- backend/spring-boot/src/main/java/org/bootstrapbugz/api/auth/payload/request/VerificationEmailRequest.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/api/auth/payload/request/VerificationEmailRequest.java
@@ -1,6 +1,7 @@
 package org.bootstrapbugz.api.auth.payload.request;
 
+import jakarta.validation.constraints.NotNull;
 import org.bootstrapbugz.api.shared.validator.UsernameOrEmail;
 
 public record VerificationEmailRequest(
-    @UsernameOrEmail(message = "{usernameOrEmail.invalid}") String usernameOrEmail) {}
+    @NotNull @UsernameOrEmail(message = "{usernameOrEmail.invalid}") String usernameOrEmail) {}
