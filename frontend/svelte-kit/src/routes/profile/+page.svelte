674f97e2ca6a8be7dd2833cde1e944156ed70c2e
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-01-06 23:55:15 +0100

    svelte - user/profile page

--- frontend/svelte-kit/src/routes/profile/+page.svelte
+++ frontend/svelte-kit/src/routes/profile/+page.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+  import avatar from '$lib/images/avatar.jpg';
   import { userStore } from '$lib/stores/user';
 </script>
 
-<div class="hero min-h-screen">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      {#if $userStore}
-        <h1 class="text-2xl font-bold">Profile page of: {$userStore.username}</h1>
-      {/if}
+<div class="flex h-screen flex-col items-center justify-center">
+  <div class="card mx-auto w-full max-w-xl bg-base-100 p-8 shadow-xl">
+    <div class="flex justify-center">
+      <img class="mt-6 h-32 w-32 rounded-full" src={avatar} alt="Avatar" />
+    </div>
+    <div class="mb-4 mt-6 text-center">
+      <h1 class="text-2xl font-bold">{$userStore?.username}</h1>
+      <p class="text-sm">{`${$userStore?.firstName} ${$userStore?.lastName}`}</p>
+    </div>
+    <div class="px-6 py-4">
+      <p class="text-base">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque semper est, in
+        finibus turpis commodo ut.
+      </p>
     </div>
   </div>
 </div>
--- frontend/svelte-kit/src/routes/user/[name]/+page.svelte
+++ frontend/svelte-kit/src/routes/user/[name]/+page.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
+  import avatar from '$lib/images/avatar.jpg';
   import type { PageServerData } from './$types';
 
   export let data: PageServerData;
 </script>
 
-<div class="hero min-h-screen">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <p class="py-6">{data?.user?.firstName}</p>
-      <p class="py-6">{data?.user?.lastName}</p>
-      <p class="py-6">{data?.user?.username}</p>
-      {#if data?.user?.email}
-        <p class="py-6">{data.user.email}</p>
-      {/if}
+<div class="flex h-screen flex-col items-center justify-center">
+  <div class="card mx-auto w-full max-w-xl bg-base-100 p-8 shadow-xl">
+    <div class="flex justify-center">
+      <img class="mt-6 h-32 w-32 rounded-full" src={avatar} alt="Avatar" />
+    </div>
+    <div class="mb-4 mt-6 text-center">
+      <h1 class="text-2xl font-bold">{data.user.username}</h1>
+      <p class="text-sm">{`${data.user.firstName} ${data.user.lastName}`}</p>
+    </div>
+    <div class="px-6 py-4">
+      <p class="text-base">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pellentesque semper est, in
+        finibus turpis commodo ut.
+      </p>
     </div>
   </div>
 </div>
