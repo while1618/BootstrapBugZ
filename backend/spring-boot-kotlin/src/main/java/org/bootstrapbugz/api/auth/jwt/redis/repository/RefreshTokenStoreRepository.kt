cc0fa438f5202f5254bf5518d85fa70d908e9498
Andrija Svrkota <andrija.svrkota@gmail.com>
2024-01-21 23:29:36 +0100

    Migrated repositories to kotlin

---
+++ backend/spring-boot-kotlin/src/main/java/org/bootstrapbugz/api/auth/jwt/redis/repository/RefreshTokenStoreRepository.kt
@@ -0,0 +1,10 @@
+package org.bootstrapbugz.api.auth.jwt.redis.repository
+
+import org.bootstrapbugz.api.auth.jwt.redis.model.RefreshTokenStore
+import org.springframework.data.repository.CrudRepository
+import java.util.Optional
+
+interface RefreshTokenStoreRepository : CrudRepository<RefreshTokenStore, String> {
+    fun findByUserIdAndIpAddress(userId: Long, ipAddress: String): Optional<RefreshTokenStore>
+    fun findAllByUserId(userId: Long): List<RefreshTokenStore>
+}
