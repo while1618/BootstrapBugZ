1c86eda3c8719cff8eae0e99efc30bce8a7bf240
Andrija Svrkota <andrija.svrkota@gmail.com>
2024-01-24 23:12:42 +0100

    Further migrations

---
+++ backend/spring-boot-kotlin/src/main/java/org/bootstrapbugz/api/admin/controller/UserController.kt
@@ -0,0 +1,24 @@
+package org.bootstrapbugz.api.admin.controller
+
+import jakarta.validation.Valid
+import org.bootstrapbugz.api.admin.payload.request.PatchUserRequest
+import org.bootstrapbugz.api.admin.payload.request.UserRequest
+import org.bootstrapbugz.api.admin.service.UserService
+import org.bootstrapbugz.api.shared.constants.Path
+import org.bootstrapbugz.api.shared.generic.crud.CrudController
+import org.bootstrapbugz.api.user.payload.dto.UserDTO
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("adminUserController")
+@RequestMapping(Path.ADMIN_USERS)
+class UserController(private val userService: UserService) : CrudController<UserDTO, UserRequest>(userService) {
+    @PatchMapping("/{id}")
+    fun patch(@PathVariable("id") id: Long, @Valid @RequestBody patchUserRequest: PatchUserRequest): ResponseEntity<UserDTO> {
+        return ResponseEntity.ok(userService.patch(id, patchUserRequest))
+    }
+}
---
+++ backend/spring-boot-kotlin/src/main/java/org/bootstrapbugz/api/user/controller/ProfileController.kt
@@ -0,0 +1,41 @@
+package org.bootstrapbugz.api.user.controller
+
+import jakarta.validation.Valid
+import org.bootstrapbugz.api.shared.constants.Path
+import org.bootstrapbugz.api.user.payload.dto.UserDTO
+import org.bootstrapbugz.api.user.payload.request.ChangePasswordRequest
+import org.bootstrapbugz.api.user.payload.request.PatchProfileRequest
+import org.bootstrapbugz.api.user.service.ProfileService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(Path.PROFILE)
+class ProfileController(private val profileService: ProfileService) {
+    @GetMapping
+    fun find(): ResponseEntity<UserDTO> {
+        return ResponseEntity.ok(profileService.find())
+    }
+
+    @PatchMapping
+    fun patch(@Valid @RequestBody patchProfileRequest: PatchProfileRequest): ResponseEntity<UserDTO> {
+        return ResponseEntity.ok(profileService.patch(patchProfileRequest))
+    }
+
+    @DeleteMapping
+    fun delete(): ResponseEntity<Void> {
+        profileService.delete()
+        return ResponseEntity.noContent().build()
+    }
+
+    @PatchMapping("/password")
+    fun changePassword(@RequestBody changePasswordRequest: @Valid ChangePasswordRequest): ResponseEntity<Void> {
+        profileService.changePassword(changePasswordRequest)
+        return ResponseEntity.noContent().build()
+    }
+}
