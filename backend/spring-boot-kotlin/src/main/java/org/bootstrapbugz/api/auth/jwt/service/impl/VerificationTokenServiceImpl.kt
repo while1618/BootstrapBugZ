7995cdb02eff6aa2db5ed4ba96602017077f2a82
Andrija Svrkota <andrija.svrkota@gmail.com>
2024-02-10 16:16:54 +0100

    Refactored JWT service classes

---
+++ backend/spring-boot-kotlin/src/main/java/org/bootstrapbugz/api/auth/jwt/service/impl/VerificationTokenServiceImpl.kt
@@ -0,0 +1,47 @@
+package org.bootstrapbugz.api.auth.jwt.service.impl
+
+import com.auth0.jwt.JWT
+import org.bootstrapbugz.api.auth.jwt.event.OnSendJwtEmail
+import org.bootstrapbugz.api.auth.jwt.service.VerificationTokenService
+import org.bootstrapbugz.api.auth.jwt.util.JwtUtil
+import org.bootstrapbugz.api.auth.jwt.util.JwtUtil.JwtPurpose
+import org.bootstrapbugz.api.shared.error.exception.BadRequestException
+import org.bootstrapbugz.api.user.model.User
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.stereotype.Service
+import java.time.Instant
+
+@Service
+class VerificationTokenServiceImpl(
+    private val eventPublisher: ApplicationEventPublisher,
+    @Value("\${jwt.secret}") private val secret: String,
+    @Value("\${jwt.verify-email-token.duration}") private val tokenDuration: Int
+) : VerificationTokenService {
+
+    companion object {
+        private val PURPOSE = JwtPurpose.VERIFY_EMAIL_TOKEN
+    }
+
+    override fun create(userId: Long?): String {
+        return JWT.create()
+            .withIssuer(userId.toString())
+            .withClaim("purpose", PURPOSE.name)
+            .withIssuedAt(Instant.now())
+            .withExpiresAt(Instant.now().plusSeconds(tokenDuration.toLong()))
+            .sign(JwtUtil.getAlgorithm(secret))
+    }
+
+    override fun check(token: String) {
+        try {
+            JwtUtil.verify(token, secret, PURPOSE)
+        } catch (e: RuntimeException) {
+            throw BadRequestException("token", e.message)
+        }
+    }
+
+    override fun sendToEmail(user: User, token: String) {
+        eventPublisher.publishEvent(OnSendJwtEmail(user, token, PURPOSE))
+    }
+
+}
