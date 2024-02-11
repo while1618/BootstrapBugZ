a01c28864c3ca2717e89bac714f905187c9ce655
Andrija Svrkota <andrija.svrkota@gmail.com>
2024-02-11 14:45:57 +0100

    Further refactors

---
+++ backend/spring-boot-kotlin/src/main/java/org/bootstrapbugz/api/auth/util/AuthUtil.kt
@@ -0,0 +1,37 @@
+package org.bootstrapbugz.api.auth.util
+
+import jakarta.servlet.http.HttpServletRequest
+import org.bootstrapbugz.api.auth.security.UserPrincipal
+import org.bootstrapbugz.api.user.model.Role.RoleName
+import org.springframework.http.HttpHeaders
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+
+object AuthUtil {
+    fun isSignedIn(): Boolean {
+        val auth = SecurityContextHolder.getContext().authentication
+        return auth.principal != "anonymousUser"
+    }
+
+    fun isAdminSignedIn(): Boolean {
+        if (!isSignedIn()) return false
+        val userPrincipal = findSignedInUser()
+        return userPrincipal.authorities?.any { authority ->
+            authority.authority == RoleName.ADMIN.name
+        } ?: false
+    }
+
+    fun findSignedInUser(): UserPrincipal {
+        val auth = SecurityContextHolder.getContext().authentication
+        return auth.principal as UserPrincipal
+    }
+
+    fun getUserIpAddress(request: HttpServletRequest): String {
+        val ipAddress = request.getHeader("x-forwarded-for")
+        return if (ipAddress == null || ipAddress.isEmpty()) request.remoteAddr else ipAddress
+    }
+
+    fun getAccessTokenFromRequest(request: HttpServletRequest): String {
+        return request.getHeader(HttpHeaders.AUTHORIZATION)
+    }
+}
---
+++ backend/spring-boot-kotlin/src/main/java/org/bootstrapbugz/api/shared/error/handling/CustomAuthenticationEntryPoint.kt
@@ -0,0 +1,34 @@
+package org.bootstrapbugz.api.shared.error.handling
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import lombok.extern.slf4j.Slf4j
+import org.bootstrapbugz.api.shared.payload.dto.ErrorMessage
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.stereotype.Component
+import java.io.IOException
+
+@Component
+class CustomAuthenticationEntryPoint : AuthenticationEntryPoint {
+
+    companion object {
+        private val log = LoggerFactory.getLogger(CustomAuthenticationEntryPoint::class.java)
+    }
+
+    override fun commence(request: HttpServletRequest?, response: HttpServletResponse, e: AuthenticationException) {
+        try {
+            val errorMessage = ErrorMessage(HttpStatus.UNAUTHORIZED).apply {
+                addDetails(e.message ?: "Unauthorized")
+            }
+            response.contentType = MediaType.APPLICATION_PROBLEM_JSON_VALUE
+            response.status = HttpStatus.UNAUTHORIZED.value()
+            response.outputStream.println(errorMessage.toString())
+        } catch (ex: IOException) {
+            log.error(ex.message ?: "An error occurred while processing the authentication entry point.")
+        }
+    }
+}
