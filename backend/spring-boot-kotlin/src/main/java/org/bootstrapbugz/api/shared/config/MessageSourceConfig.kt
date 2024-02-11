7cdeb410059b2c22253df337c798213d9fbda0d6
Andrija Svrkota <andrija.svrkota@gmail.com>
2024-02-11 14:32:31 +0100

    Migrated configuration classes

---
+++ backend/spring-boot-kotlin/src/main/java/org/bootstrapbugz/api/shared/config/MessageSourceConfig.kt
@@ -0,0 +1,17 @@
+package org.bootstrapbugz.api.shared.config
+
+import org.springframework.context.MessageSource
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
+
+@Configuration
+class MessageSourceConfig {
+    @Bean
+    fun messageSource(): MessageSource {
+        val messageSource = ReloadableResourceBundleMessageSource()
+        messageSource.setBasename("classpath:/ValidationMessages")
+        messageSource.setDefaultEncoding("UTF-8")
+        return messageSource
+    }
+}
---
+++ backend/spring-boot-kotlin/src/main/java/org/bootstrapbugz/api/shared/config/WebMvcConfig.kt
@@ -0,0 +1,16 @@
+package org.bootstrapbugz.api.shared.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+            .maxAge(3600)
+    }
+}
