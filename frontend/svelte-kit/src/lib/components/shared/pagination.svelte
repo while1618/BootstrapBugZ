757c2c074e95aa73857daca624e35f15742f662b
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-02-24 02:25:00 +0100

    sveltekit: pagination ui redesign

--- frontend/svelte-kit/src/lib/components/shared/pagination.svelte
+++ frontend/svelte-kit/src/lib/components/shared/pagination.svelte
@@ -6,40 +6,28 @@
   export let size: number;
 </script>
 
-<div class="join">
+<div class="gap-2">
   {#if currentPage > 1}
-    <a
-      class="btn btn-primary join-item"
-      href="{$page.url.pathname}?page={currentPage - 1}&size={size}"
-    >
+    <a class="btn btn-ghost" href="{$page.url.pathname}?page={currentPage - 1}&size={size}">
       Previous
     </a>
   {/if}
   {#each [2, 1] as i}
     {#if currentPage - i > 0}
-      <a
-        class="btn btn-primary join-item"
-        href="{$page.url.pathname}?page={currentPage - i}&size={size}"
-      >
+      <a class="btn btn-ghost" href="{$page.url.pathname}?page={currentPage - i}&size={size}">
         {currentPage - i}
       </a>
     {/if}
   {/each}
-  <span class="btn btn-primary join-item btn-active">{currentPage}</span>
+  <span class="btn btn-primary btn-active">{currentPage}</span>
   {#each Array(2) as _, i}
     {#if currentPage + (i + 1) <= totalPages}
-      <a
-        class="btn btn-primary join-item"
-        href="{$page.url.pathname}?page={currentPage + (i + 1)}&size={size}"
-      >
+      <a class="btn btn-ghost" href="{$page.url.pathname}?page={currentPage + (i + 1)}&size={size}">
         {currentPage + (i + 1)}
       </a>
     {/if}
   {/each}
   {#if currentPage < totalPages}
-    <a
-      class="btn btn-primary join-item"
-      href="{$page.url.pathname}?page={currentPage + 1}&size={size}">Next</a
-    >
+    <a class="btn btn-ghost" href="{$page.url.pathname}?page={currentPage + 1}&size={size}">Next</a>
   {/if}
 </div>
