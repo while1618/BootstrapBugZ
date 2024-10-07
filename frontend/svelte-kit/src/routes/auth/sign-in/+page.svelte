3c42eb5804b976d6272f86142d7dedcf17b6538d
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-10-08 00:48:10 +0200

    missing frontend part

--- frontend/svelte-kit/src/routes/auth/sign-in/+page.svelte
+++ frontend/svelte-kit/src/routes/auth/sign-in/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
   import FormControl from '$lib/components/form/form-control.svelte';
+  import FormErrors from '$lib/components/form/form-errors.svelte';
   import * as m from '$lib/paraglide/messages.js';
   import type { ActionData } from './$types';
 
@@ -9,27 +10,20 @@
 
 <section class="py-10 md:py-16">
   <div class="container">
-    <div class="card mx-auto w-full max-w-xl bg-base-200 p-8 shadow-xl">
+    <div class="card bg-base-200 mx-auto w-full max-w-xl p-8 shadow-xl">
       <div class="flex flex-col gap-8">
         <h1 class="text-center text-3xl font-bold">{m.auth_signIn()}</h1>
         <form class="flex flex-col gap-4" method="POST" action="?/signIn" use:enhance>
           <FormControl {form} type="text" name="usernameOrEmail" label={m.auth_usernameOrEmail()} />
           <FormControl {form} type="password" name="password" label={m.auth_password()} />
-
-          {#if form?.errorMessage}
-            {#each form.errorMessage.details as error}
-              <div class="flex gap-4">
-                <p class="label-text text-error">{error.message}</p>
-                {#if error.message === 'User not activated.'}
-                  <a
-                    href="/auth/resend-confirmation-email?usernameOrEmail={form.usernameOrEmail}"
-                    class="label-text text-info hover:underline"
-                  >
-                    {m.auth_resendConfirmationEmail()}
-                  </a>
-                {/if}
-              </div>
-            {/each}
+          <FormErrors {form} />
+          {#if form?.errorMessage && form.errorMessage.codes.includes('api_user_notActive')}
+            <a
+              href="/auth/resend-confirmation-email?usernameOrEmail={form.usernameOrEmail}"
+              class="label-text text-info hover:underline"
+            >
+              {m.auth_resendConfirmationEmail()}
+            </a>
           {/if}
 
           <button class="btn btn-primary">{m.auth_signIn()}</button>
@@ -40,7 +34,7 @@
                 {m.auth_signUp()}
               </a>
             </span>
-            <a href="/auth/forgot-password" class="label-text ml-auto text-info hover:underline">
+            <a href="/auth/forgot-password" class="label-text text-info ml-auto hover:underline">
               {m.auth_forgotPassword()}
             </a>
           </div>
