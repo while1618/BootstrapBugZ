4e3b4e0f9c4b9c47f96d27660d050efe77ebff4f
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-10-23 00:44:27 +0200

    springboot: HHH000104 comment

--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/user/repository/UserRepository.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/user/repository/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  @Query("SELECT u.id FROM User u")
+  @Query("select u.id from User u")
   List<Long> findAllUserIds(Pageable pageable);
 
   @EntityGraph(attributePaths = "roles")
