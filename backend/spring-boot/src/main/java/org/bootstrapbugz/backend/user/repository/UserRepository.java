63caff42e33bc463f5768d759bef44dcd876bcaf
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-10-22 13:30:34 +0200

    springboot: fix_HHH90003004 by two separated queries

--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/user/repository/UserRepository.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/user/repository/UserRepository.java
@@ -1,11 +1,20 @@
 package org.bootstrapbugz.backend.user.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.bootstrapbugz.backend.user.model.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+  @Query("SELECT u.id FROM User u")
+  List<Long> findUserIds(Pageable pageable);
+
+  @EntityGraph(attributePaths = "roles")
+  List<User> findByIdIn(List<Long> ids);
+
   @EntityGraph(attributePaths = "roles")
   Optional<User> findWithRolesById(Long id);
 
