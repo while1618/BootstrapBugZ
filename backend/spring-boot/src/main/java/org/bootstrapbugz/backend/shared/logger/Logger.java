d81b16c83a194739d5570bd4fd4f80d2353695e9
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-10-29 03:10:59 +0100

    springboot: add request id to log

--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/Logger.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/Logger.java
@@ -3,6 +3,7 @@ package org.bootstrapbugz.backend.shared.logger;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.bootstrapbugz.backend.auth.util.AuthUtil;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -13,7 +14,8 @@ public class Logger {
   public void info(String message) {
     final var logDetails = new LogDetails();
     log.info(
-        "USER: {}, IP: {}, REQUEST: {} {}, MESSAGE: {}",
+        "REQUEST_ID: {}, USER: {}, IP: {}, ENDPOINT: {} {}, MESSAGE: {}",
+        logDetails.getRequestId(),
         logDetails.getUsername(),
         logDetails.getIpAddress(),
         logDetails.getRequestMethod(),
@@ -24,7 +26,8 @@ public class Logger {
   public void error(String message, Exception e) {
     final var logDetails = new LogDetails();
     log.error(
-        "USER: {}, IP: {}, REQUEST: {} {}, MESSAGE: {}",
+        "REQUEST_ID: {}, USER: {}, IP: {}, ENDPOINT: {} {}, MESSAGE: {}",
+        logDetails.getRequestId(),
         logDetails.getUsername(),
         logDetails.getIpAddress(),
         logDetails.getRequestMethod(),
@@ -37,6 +40,7 @@ public class Logger {
   private static class LogDetails {
     private final String username;
     private final String ipAddress;
+    private final String requestId;
     private final String requestMethod;
     private final String requestUrl;
 
@@ -45,6 +49,7 @@ public class Logger {
           ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
       username = AuthUtil.getAuthName();
       ipAddress = AuthUtil.getUserIpAddress(request);
+      requestId = MDC.get("REQUEST_ID");
       requestMethod = request.getMethod();
       requestUrl = request.getRequestURL().toString();
     }
