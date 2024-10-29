44fa65dfd70e386adaf408270712bfa131c99e79
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-10-29 16:02:01 +0100

    springboot: add request_id to response

--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/MDCInterceptor.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/MDCInterceptor.java
@@ -1,5 +1,6 @@
 package org.bootstrapbugz.backend.shared.logger;
 
+import com.google.common.net.HttpHeaders;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.UUID;
@@ -12,7 +13,9 @@ public class MDCInterceptor implements HandlerInterceptor {
   @Override
   public boolean preHandle(
       HttpServletRequest request, HttpServletResponse response, Object handler) {
-    MDC.put("REQUEST_ID", UUID.randomUUID().toString());
+    final var requestId = UUID.randomUUID().toString();
+    response.setHeader(HttpHeaders.X_REQUEST_ID, requestId);
+    MDC.put("REQUEST_ID", requestId);
     return true;
   }
 
