3b8b2168294c3e3b7b3741031b9037db13c2bfb8
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-10-29 02:34:27 +0100

    springboot: LogDetails

--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/Logger.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/Logger.java
@@ -1,5 +1,6 @@
 package org.bootstrapbugz.backend.shared.logger;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.bootstrapbugz.backend.auth.util.AuthUtil;
 import org.springframework.stereotype.Component;
@@ -10,35 +11,42 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 @Component
 public class Logger {
   public void info(String message) {
-    final var request =
-        ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-    final var username = AuthUtil.getAuthName();
-    final var ipAddress = AuthUtil.getUserIpAddress(request);
-    final var method = request.getMethod();
-    final var endpoint = request.getRequestURL().toString();
+    final var logDetails = new LogDetails();
     log.info(
         "USER: {}, IP: {}, REQUEST: {} {}, MESSAGE: {}",
-        username,
-        ipAddress,
-        method,
-        endpoint,
+        logDetails.getUsername(),
+        logDetails.getIpAddress(),
+        logDetails.getRequestMethod(),
+        logDetails.getRequestUrl(),
         message);
   }
 
   public void error(String message, Exception e) {
-    final var request =
-        ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-    final var username = AuthUtil.getAuthName();
-    final var ipAddress = AuthUtil.getUserIpAddress(request);
-    final var method = request.getMethod();
-    final var endpoint = request.getRequestURL().toString();
+    final var logDetails = new LogDetails();
     log.error(
         "USER: {}, IP: {}, REQUEST: {} {}, MESSAGE: {}",
-        username,
-        ipAddress,
-        method,
-        endpoint,
+        logDetails.getUsername(),
+        logDetails.getIpAddress(),
+        logDetails.getRequestMethod(),
+        logDetails.getRequestUrl(),
         message,
         e);
+  }
+
+  @Getter
+  private static class LogDetails {
+    private final String username;
+    private final String ipAddress;
+    private final String requestMethod;
+    private final String requestUrl;
+
+    public LogDetails() {
+      final var request =
+          ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+      username = AuthUtil.getAuthName();
+      ipAddress = AuthUtil.getUserIpAddress(request);
+      requestMethod = request.getMethod();
+      requestUrl = request.getRequestURL().toString();
+    }
   }
 }
