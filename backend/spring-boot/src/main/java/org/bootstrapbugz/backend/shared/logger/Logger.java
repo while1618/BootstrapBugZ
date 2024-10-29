ab41eec5f7d78cebbdc7179a9dd5a3c6562f1b07
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-10-29 01:02:09 +0100

    springboot: refactor logger

--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/Logger.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/Logger.java
@@ -1,17 +1,25 @@
 package org.bootstrapbugz.backend.shared.logger;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.bootstrapbugz.backend.auth.util.AuthUtil;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Slf4j
 public class Logger {
-  private String username;
-  private String ipAddress;
-  private String method;
-  private String endpoint;
+  private final String username;
+  private final String ipAddress;
+  private final String method;
+  private final String endpoint;
+
+  public Logger() {
+    final var request =
+        ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+    username = AuthUtil.getAuthName();
+    ipAddress = AuthUtil.getUserIpAddress(request);
+    method = request.getMethod();
+    endpoint = request.getRequestURL().toString();
+  }
 
   public void info(String message) {
     log.info(
--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/LoggingAspect.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/logger/LoggingAspect.java
@@ -7,12 +7,9 @@ import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.bootstrapbugz.backend.auth.util.AuthUtil;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Aspect
 @Component
@@ -33,45 +30,35 @@ public class LoggingAspect {
       "execution(public * org.bootstrapbugz.backend.shared.error.handling.CustomAuthenticationEntryPoint.commence(..))")
   public void authEntryPoint() {}
 
-  private Logger createLogger() {
-    final var request =
-        ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-    final var username = AuthUtil.getAuthName();
-    final var ipAddress = AuthUtil.getUserIpAddress(request);
-    final var method = request.getMethod();
-    final var endpoint = request.getRequestURL().toString();
-    return new Logger(username, ipAddress, method, endpoint);
-  }
-
   @Before(value = "controllerLayer()")
   public void logBefore() {
-    final var logger = createLogger();
+    final var logger = new Logger();
     logger.info("Called");
   }
 
   @AfterReturning(value = "controllerLayer()")
   public void logAfter() {
-    final var logger = createLogger();
+    final var logger = new Logger();
     logger.info("Finished");
   }
 
   @AfterThrowing(pointcut = "controllerLayer()", throwing = "e")
   public void logException(Exception e) {
-    final var logger = createLogger();
+    final var logger = new Logger();
     logger.error("Thrown", e);
   }
 
   @Before(value = "handleMethodArgumentNotValid()")
   public void beforeHandleMethodArgumentNotValid(JoinPoint joinPoint) {
     final var e = (MethodArgumentNotValidException) joinPoint.getArgs()[0];
-    final var logger = createLogger();
+    final var logger = new Logger();
     logger.error("Invalid arguments", e);
   }
 
   @Before(value = "authEntryPoint()")
   public void beforeAuthEntryPoint(JoinPoint joinPoint) {
     final var e = (AuthenticationException) joinPoint.getArgs()[2];
-    final var logger = createLogger();
+    final var logger = new Logger();
     logger.error("Auth failed", e);
   }
 }
