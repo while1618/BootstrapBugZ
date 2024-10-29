a2c3b0caa556fe0c0e958c9bb0ba80eb334e5af8
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-10-29 21:40:51 +0100

    springboot: add request_id to auth failed handler

--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/error/handling/CustomAuthenticationEntryPoint.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/shared/error/handling/CustomAuthenticationEntryPoint.java
@@ -1,11 +1,14 @@
 package org.bootstrapbugz.backend.shared.error.handling;
 
+import com.google.common.net.HttpHeaders;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.UUID;
 import org.bootstrapbugz.backend.shared.error.ErrorMessage;
 import org.bootstrapbugz.backend.shared.logger.CustomLogger;
 import org.bootstrapbugz.backend.shared.message.service.MessageService;
+import org.slf4j.MDC;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
@@ -26,11 +29,15 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
   public void commence(
       HttpServletRequest request, HttpServletResponse response, AuthenticationException e)
       throws IOException {
-    customLogger.error("Auth failed", e);
+    final var requestId = UUID.randomUUID().toString();
+    MDC.put("REQUEST_ID", requestId);
     final var errorMessage = new ErrorMessage(HttpStatus.UNAUTHORIZED);
     errorMessage.addCode(messageService.getMessage("auth.unauthorized"));
+    response.setHeader(HttpHeaders.X_REQUEST_ID, requestId);
     response.setContentType(MediaType.APPLICATION_PROBLEM_JSON_VALUE);
     response.setStatus(HttpStatus.UNAUTHORIZED.value());
     response.getOutputStream().println(errorMessage.toString());
+    customLogger.error("Auth failed", e);
+    MDC.remove("REQUEST_ID");
   }
 }
