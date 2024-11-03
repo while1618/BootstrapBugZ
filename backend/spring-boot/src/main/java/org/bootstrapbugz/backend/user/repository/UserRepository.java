64aa59e8a858a6a562a9d9e08232a3833cb5de49
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-11-03 02:02:20 +0100

    springboot: findAllUserIds order by id

--- backend/spring-boot/src/main/java/org/bootstrapbugz/backend/user/repository/UserRepository.java
+++ backend/spring-boot/src/main/java/org/bootstrapbugz/backend/user/repository/UserRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  @Query("select u.id from User u")
+  @Query("select u.id from User u order by u.id")
   List<Long> findAllUserIds(Pageable pageable);
 
   @EntityGraph(attributePaths = "roles")
