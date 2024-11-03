5c43fe5fa5c03d0f89ffea2dfca104dc758ad0f3
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-11-03 03:08:30 +0100

    sveltekit: mandatory body and action in modal

--- frontend/svelte-kit/src/lib/components/shared/modal.svelte
+++ frontend/svelte-kit/src/lib/components/shared/modal.svelte
@@ -2,8 +2,8 @@
   interface Props {
     dialog: HTMLDialogElement;
     title: string;
-    body?: import('svelte').Snippet;
-    actions?: import('svelte').Snippet;
+    body: import('svelte').Snippet;
+    actions: import('svelte').Snippet;
   }
 
   let { dialog = $bindable(), title, body, actions }: Props = $props();
@@ -12,9 +12,9 @@
 <dialog class="modal modal-bottom sm:modal-middle" bind:this={dialog}>
   <div class="modal-box">
     <h3 class="text-lg font-bold">{title}</h3>
-    {@render body?.()}
+    {@render body()}
     <div class="modal-action">
-      {@render actions?.()}
+      {@render actions()}
     </div>
   </div>
 </dialog>
