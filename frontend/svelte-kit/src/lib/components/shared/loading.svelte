41c402a8f6d81feef84111b41885caaee19ac67a
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-11-18 01:58:43 +0100

    sveltekit: loader

--- frontend/svelte-kit/src/lib/components/shared/loading.svelte
+++ frontend/svelte-kit/src/lib/components/shared/loading.svelte
@@ -1,1 +1,7 @@
-<p>loading</p>
+<script>
+  import { LoaderCircleIcon } from 'lucide-svelte';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center">
+  <LoaderCircleIcon size={32} strokeWidth={2} class="animate-spin" />
+</div>
