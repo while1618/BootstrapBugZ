aa45b894bc90e4d683d3a4280ad11d40492a0d7f
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2025-01-02 01:16:08 +0100

    init script

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ rm -- "$0"
 
 clear
 echo "What is the name of your app: "
-APP_NAME=$(gum input --placeholder "")
+APP_NAME=$(gum input --placeholder "" | tr '[:upper:]' '[:lower:]')
 
 clear
 echo "Choose your backend: "
@@ -25,8 +25,21 @@ cp -r ./frontend/$FRONTEND/. ./frontend
 rm -rf ./backend/$BACKEND
 rm -rf ./frontend/$FRONTEND
 
-# TODO: replace all occurences of the name in the files
-# TODO: create .env file in frontend
+if [ "$BACKEND" == "spring-boot" ]; then
+  mv ./backend/src/main/java/org/bugzkit ./backend/src/main/java/org/$APP_NAME
+  mv ./backend/src/test/java/org/bugzkit ./backend/src/test/java/org/$APP_NAME
+fi
+
+# replace all occurrences of the bugzkit in all files
+find . \
+  -type f \
+  -not -path "./node_modules/*" \
+  -not -path "./target/*" \
+  -not -path "./.git/*" \
+  -not -path "./build/*" \
+  -not -path "./init.sh" \
+  -not -path "./.svelte-kit/*" \
+  -exec sed -i "s/bugzkit/${APP_NAME}/Ig" {} + > /dev/null 2>&1
 
 clear
 echo "Name of your app is: $APP_NAME"
