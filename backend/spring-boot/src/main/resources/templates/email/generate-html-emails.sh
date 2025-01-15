824a9bf94a0d968713d6806a32d3ed00f40f7664
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2025-01-16 00:50:31 +0100

    springboot: generate html emails from mjml template

---
+++ backend/spring-boot/src/main/resources/templates/email/generate-html-emails.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+TEMPLATE_DIR="$(dirname "$0")"
+for file in "$TEMPLATE_DIR"/*.mjml; do
+  if [[ -f "$file" ]]; then
+    output="${file%.mjml}.html"
+    echo "Converting $file to $output"
+    npx mjml "$file" -o "$output"
+  fi
+done
