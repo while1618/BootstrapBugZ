94bac80028946162f1516603e43bb9c3e4082b49
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2025-01-20 00:58:20 +0100

    docs in nextra v4 - init

---
+++ new-docs/eslint.config.mjs
@@ -0,0 +1,21 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+  {
+    ignores: ['node_modules/', '.next/', 'next-env.d.ts'],
+  },
+];
+
+export default eslintConfig;
---
+++ new-docs/src/content/how-it-works/_meta.js
@@ -0,0 +1,8 @@
+const config = {
+  introduction: 'Introduction',
+  'project-structure': 'Project Structure',
+  auth: 'Auth',
+  email: 'Email',
+};
+
+export default config;
