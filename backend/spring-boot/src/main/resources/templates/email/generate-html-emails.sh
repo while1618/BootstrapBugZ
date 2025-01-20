8cde7a898a50c1b915de2581d0ebda673fe5f35b
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2025-01-20 13:02:14 +0100

    springboot: fix generate-html-emails.sh

--- backend/spring-boot/src/main/resources/templates/email/generate-html-emails.sh
+++ backend/spring-boot/src/main/resources/templates/email/generate-html-emails.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 TEMPLATE_DIR="$(dirname "$0")"
 for file in "$TEMPLATE_DIR"/*.mjml; do
-  if [[ -f "$file" ]]; then
+  if [ -f "$file" ]; then
     output="${file%.mjml}.html"
     echo "Converting $file to $output"
     npx mjml "$file" -o "$output"
